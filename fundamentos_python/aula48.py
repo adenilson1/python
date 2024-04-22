3fb01cfa5682b06b26f76c90f644d9c12e53ef73
adenilson1 <adenilson99@hotmail.com>
2024-04-22 15:15:28 -0300

    Fundamentos do python

--- fundamentos_python/aula48.py
+++ fundamentos_python/aula48.py
@@ -3,5 +3,24 @@ CONSTANTE = "Variaveis" que não vão mudar
 Muitas condições no mesmo if (ruim)
     <-Contagem de complexidade (ruim)
 """
-velocidade = 61 #velocidade atual do carro
-local_carro = 101 #local em que o carro está na estrada
+velocidade = 601#velocidade atual do carro
+local_carro = 98 #local em que o carro está na estrada
+
+RADAR_1 = 60 #velocidade máxima permitida do radar 1
+LOCAL_1 = 101 #local onde o radar 1 está
+RADAR_RANGE = 1 #A distancia onde o radar pega
+
+vel_carro_passaou_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_RANGE) and \
+     local_carro <=  (LOCAL_1 + RADAR_RANGE) 
+carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_passaou_radar_1 > RADAR_1
+
+if vel_carro_passaou_radar_1:
+    print('Velocidade do carro passou do radar 1')
+
+if carro_passou_radar_1:
+    print('Carro passou pelo radar 1')
+if carro_multado_radar_1:
+    print('O carro foi multado')
+else:
+    print('O carro não foi multado')
