4bac9dd4ae0e9c288f2166cf498dd549141017c6
adenilson1 <adenilson99@hotmail.com>
2024-04-24 15:22:37 -0300

    Fundamentos do python

--- fundamentos_python/exercicio45.py
+++ fundamentos_python/exercicio45.py
@@ -7,18 +7,18 @@ maior que 6 letras escreva "Seu nome é muito grande".
 
 """
 primeiro_nome = input('Digite o seu nome: ')
-numero_letras_do_nome = len(primeiro_nome)
+tamanho_nome = len(primeiro_nome)
 
-nome_curto = numero_letras_do_nome < 5 
-nome_normal = numero_letras_do_nome >= 5 and numero_letras_do_nome < 7
-
+nome_curto = tamanho_nome >= 1 and tamanho_nome < 5 
+nome_normal = tamanho_nome >= 5 and tamanho_nome < 7
+nome_grande = tamanho_nome >= 7
 
 if nome_curto:
     print(f'{primeiro_nome}, seu nome é curto')
 elif nome_normal:
     print(f'{primeiro_nome}, seu nome é normal')
-else:
+elif nome_grande:
     print(f'{primeiro_nome}, seu nome é grande')
+else:
+    print('Digite pelo menos uma letra')
 
-
-print(numero_letras_do_nome)
