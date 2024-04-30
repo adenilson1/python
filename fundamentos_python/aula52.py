16d3c6f4180fc1bd788b5dd63ab963516109fb42
adenilson1 <adenilson99@hotmail.com>
2024-04-30 11:50:55 -0300

    Fundamentos do python

---
+++ fundamentos_python/aula52.py
@@ -0,0 +1,13 @@
+"""
+Repetiçoes 
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Looop infinito -> Quando um codigo não tem fim
+"""
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador = contador + 1
+
+print('acabou')
