72f04c42a3dcf31bc8d79b688862f4abc6472018
adenilson1 <adenilson99@hotmail.com>
2024-05-16 14:42:22 -0300

    Fundamentos do python

---
+++ fundamentos_python/exercicio49.py
@@ -0,0 +1,31 @@
+"""Teste"""
+#1
+#start = 0
+#end = 10
+
+#while start < end:
+    #print(start)
+    #start += 1
+
+#2
+#start = 0
+#end = 10
+
+#while start < end:
+    #start += 1
+    #print(start)
+    
+#3
+linhas = 2
+colunas= 2
+
+linha = 1
+
+while linha <= linhas :
+    coluna = 1
+    while coluna <= colunas:
+        print(linha, coluna)
+        coluna += 1
+    linha += 1
+
+
