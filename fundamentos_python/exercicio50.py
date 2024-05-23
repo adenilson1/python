de6bf743e44c65f8ec23b240d9709d37ec8db5b2
adenilson1 <adenilson99@hotmail.com>
2024-05-23 12:09:31 -0300

    Fundamentos do python

---
+++ fundamentos_python/exercicio50.py
@@ -0,0 +1,26 @@
+"""JOGO DA ADIVINHAÇÃO"""
+"""ACERTER O NUMERO"""
+numero_secreto = 42
+numero_de_tentivas = 3
+
+while numero_de_tentivas > 0:
+    chute = int(input('Digite um número: '))
+    print(f'Você digitou o número {chute}')
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if acertou:
+        print('Você acertou o número secreto !!!')
+        break
+    elif maior:
+        print('Seu número é maior que o número secreto')
+    elif menor:
+        print('Seu número é menor que o número secreto')
+    else:
+        print('Digite um número inteiro')
+    
+    numero_de_tentivas -=1
+    print(f'Você tem {numero_de_tentivas} tentivas')
+print('****FIM DE JOGO****')
