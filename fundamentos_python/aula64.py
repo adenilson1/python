db2a3dadd9364381879690db2a1d70f3e9630a58
adenilson1 <adenilson99@hotmail.com>
2024-05-29 16:02:10 -0300

    Fundamentos do python

---
+++ fundamentos_python/aula64.py
@@ -0,0 +1,21 @@
+"""
+Conhecimento reutilizáveis - indices e fatiamento
+Métodos úteis:
+append() - adiciona um item ao final da lista
+insert() - adiciona um item em um indice espercifico
+pop() - apagar o ultimo item da lista
+del - apagar um item de um indice espercifico
+extend() - extende a lista
+clear() - limpa a lista
++ - concatena a lista
+create read update delete
+criar, ler, alterar, apagar = lista[i]
+"""
+
+lista = [10,20,30,40]
+#lista.append('carro') -> adiciona um item ao final da lista
+#lista.pop() -> apaga o item do fim da lista
+#del lista[-1] -> apaga o item do item -1
+#lista.clear() -> limpa a lista
+#lista.insert(0, 100) -> inseri um item 100 no indice 0
+print(lista)
